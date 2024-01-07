5cd6b25eaad2de45fff507dede09c27af40ed1ea
PizzaPartyInc <PizzaPartyInc>
2024-01-07 08:44:17 +0100

    feat: drop the coroutine approach and add docstrings

--- app/unity/Assets/Scripts/FadeScript.cs
+++ app/unity/Assets/Scripts/FadeScript.cs
@@ -2,40 +2,72 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// A component to be attached to the game object that is able to gradually change the alpha parameter 
+/// from the CanvasGroup component of the same object.
+/// </summary>
 public class FadeScript : MonoBehaviour
 {
-    public float fadeSpeed;
-    public bool fadeDirection = false;
+    /// <summary>
+    /// The speed of alpha transition. The higher the number - the faster the transition.
+    /// </summary>
+    public float fadeSpeed = 1;
+    /// <summary>
+    /// If true - fade in. If false - fade out.
+    /// </summary>
+    public bool fadeDirection = true;
+    /// <summary>
+    /// A CanvasGroup component attached to the same game object as this script.
+    /// The alpha property of this component is being manipulated by this script.
+    /// </summary>
     private CanvasGroup canvasGroup;
 
-    void Awake()
+    /// <summary>
+    /// Called when the script object is initialized.
+    /// </summary>
+    private void Awake()
     {
         canvasGroup = GetComponent<CanvasGroup>();
     }
 
-    IEnumerator FadeTo(float target)
+    /// <summary>
+    /// Switches the fade direction to true - fade in.
+    /// The Update calls handle the actual transition.
+    /// </summary>
+    public void FadeIn()
     {
-        float current = canvasGroup.alpha;
-        for (float t = 0.0f; t < 0.95f; t += Time.deltaTime / fadeSpeed)
-        {
-            canvasGroup.alpha = Mathf.MoveTowards(current, target, t);
-            yield return null;
-        }
-        yield return null;
+        fadeDirection = true;
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// Switches the fade direction to false - fade out.
+    /// The Update calls handle the actual transition.
+    /// </summary>
+    public void FadeOut()
+    {
+        fadeDirection = false;
+    }
+
+    /// <summary>
+    /// Update is called once per frame. Initiates either FadeIn or FadeOut
+    /// If alpha is already 1 and current fade direction is true (fade in) - skip transition.
+    /// If alpha is already 0 and current fade direction is false (fade out) - skip transition.
+    /// </summary>
+    private void Update()
     {
         if ((fadeDirection && canvasGroup.alpha == 1) || (!fadeDirection && canvasGroup.alpha == 0))
-        {
-            StopAllCoroutines();
             return;
-        }
 
-        if (fadeDirection)
-            StartCoroutine(FadeTo(1.0f));
-        else
-            StartCoroutine(FadeTo(0.0f));
+        FadeTo(fadeDirection ? 1.0f : 0.0f);
+    }
+
+    /// <summary>
+    /// Gradually changes the CanvasGroup alpha value to the targeted value.
+    /// </summary>
+    /// <param name="target">The target alpha value that the current alpha would be changed to over time, based on the fade speed property.</param>
+    /// <returns></returns>
+    private void FadeTo(float target)
+    {
+        canvasGroup.alpha = Mathf.MoveTowards(canvasGroup.alpha, target, Time.deltaTime * fadeSpeed);
     }
 }
