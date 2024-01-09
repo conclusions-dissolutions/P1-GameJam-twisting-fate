d73537f7b04fb12d5b9251f9e33cbbde669f1415
PizzaPartyInc <PizzaPartyInc>
2024-01-09 19:59:51 +0100

    chore: improve stars script

--- app/unity/Assets/Scripts/StarsScript.cs
+++ app/unity/Assets/Scripts/StarsScript.cs
@@ -16,23 +16,62 @@ public class StarsScript : MonoBehaviour
     /// </summary>
     public List<Object> starPrefabs;
 
+    /// <summary>
+    /// Seed for the stars generation. If less than 0 - generate stars randomly.
+    /// </summary>
+    public int seed = -1;
 
     /// <summary>
-    /// Goes through coordinates of the main game screen area from bottom left corner to top right corner.
-    /// Populates small areas of background with a random number of star prefabs. 0-5 stars per area.
+    /// Maximum amount of stars per sector.
+    /// </summary>
+    public int maxStarsPerSector = 10;
+
+    /// <summary>
+    /// The X coordinate of the lower left corner of the game screen.
+    /// </summary>
+    private readonly float lowerLeftCornerX = -8.8f;
+
+    /// <summary>
+    /// The Y coordinate of the lower left corner of the game screen.
+    /// </summary>
+    private readonly float lowerLeftCornerY = -4.9f;
+
+    /// <summary>
+    /// Somewhat reasonable width of the sector of the background to populate with starts to achieve relatively spread-out population.
+    /// Screen width can be split into 10 equal segments with sectorWidth as width.
+    /// </summary>
+    private readonly float sectorWidth = 1.76f;
+
+    /// <summary>
+    /// Somewhat reasonable height of the sector of the background to populate with starts to achieve relatively spread-out population.
+    /// Screen height can be split into 5 equal segments with sectorHeight as height.
+    /// </summary>
+    private readonly float sectorHeight = 1.96f;
+
+    /// <summary>
+    /// Goes through coordinates of the main game screen sector from bottom left corner to top right corner.
+    /// Populates small sectors of background with a random number of star prefabs. 0-5 stars per sector.
     /// </summary>
     public void Awake()
     {
-        float xStep = 1.76f;
-        float yStep = 1.96f;
-        for (float x = -8.8f; x <= 8.8f - xStep; x += xStep)
+        if (seed >= 0)
+            Random.InitState(seed);
+
+        // Traverse the X coordinate. The last X coordinate is located 1 xStep away from the right edge of the screen.
+        for (float x = lowerLeftCornerX; x <= lowerLeftCornerX * (-1) - sectorWidth; x += sectorWidth)
         {
-            for (float y = -4.9f; y <= 4.9f - yStep; y += yStep)
+            // Traverse the Y coordinate. The last Y coordinate is located 1 yStep away from the right edge of the screen.
+            for (float y = lowerLeftCornerY; y <= lowerLeftCornerY * (-1) - sectorHeight; y += sectorHeight)
             {
-                int starsPerAre = Random.Range(0, 6);
-                for (int i = 0; i < starsPerAre; i++)
+                // Place from 0 to configurable number of stars per sector to have a somewhat realistic-looking starry view.
+                int starsPerSector = Random.Range(0, maxStarsPerSector + 1);
+                for (int i = 0; i < starsPerSector; i++)
                 {
-                    PlaceStar(Random.Range(x, x + xStep), Random.Range(y, y + yStep), Random.Range(0, starPrefabs.Count));
+                    PlaceStar(
+                        x: Random.Range(x, x + sectorWidth),
+                        y: Random.Range(y, y + sectorHeight),
+                        starIndex: Random.Range(0, starPrefabs.Count),
+                        starScale: Random.Range(0.05f, 0.10f));
                 }
             }
         }
@@ -45,8 +84,10 @@ public class StarsScript : MonoBehaviour
     /// <param name="x">x coordinate of the star placement.</param>
     /// <param name="y">y coordinate of the star placement.</param>
     /// <param name="starIndex">An index in the list to get a star prefab.</param>
-    private void PlaceStar(float x, float y, int starIndex)
+    /// <param name="starScale">A scale of the star to be rendered used for both X and Y values.</param>
+    private void PlaceStar(float x, float y, int starIndex, float starScale)
     {
-        Instantiate(starPrefabs[starIndex], new Vector3(x, y, 0), Quaternion.identity, background.transform);
+        GameObject star = Instantiate(starPrefabs[starIndex], new Vector3(x, y, 0), Quaternion.identity, background.transform) as GameObject;
+        star.transform.localScale = new Vector3(starScale, starScale);
     }
 }
