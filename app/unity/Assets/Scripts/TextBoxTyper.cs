41d30728915b26ea0cfb00d6abe71e81656e067e
Endrophes <Endrophes@users.noreply.github.com>
2024-01-11 23:50:29 -0500

    Created TextBoxTyper
    Added a script that will print out one character at a time.

---
+++ app/unity/Assets/Scripts/TextBoxTyper.cs
@@ -0,0 +1,101 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextBoxTyper : MonoBehaviour
+{
+    /// <summary>
+    /// What will be displayed char by char
+    /// </summary>
+    public string textToPrint;
+
+    /// <summary>
+    /// Seconds till the next character
+    /// </summary>
+    public float speed;
+
+    /// <summary>
+    /// indicates if the text has been fully printed
+    /// </summary>
+    private bool isTyping = true;
+
+    /// <summary>
+    /// interface for isTyping
+    /// </summary>
+    public bool IsTyping { get { return isTyping; } }
+
+    /// <summary>
+    /// Holds how many seconds have passed from when the last char was printed
+    /// </summary>
+    private float secFromLast;
+
+    /// <summary>
+    /// Test renderer for the text box
+    /// </summary>
+    [SerializeField]
+    private TMPro.TextMeshProUGUI _textMeshProUGUI;
+
+    /// <summary>
+    /// Gets and sets the render and the transforms of the children
+    /// </summary>
+    public TMPro.TextMeshProUGUI TextMeshPro
+    {
+        get
+        {
+            if (_textMeshProUGUI == null && transform.GetComponentInChildren<TMPro.TextMeshProUGUI>())
+            {
+                _textMeshProUGUI = transform.GetComponentInChildren<TMPro.TextMeshProUGUI>();
+            }
+
+            return _textMeshProUGUI;
+        }
+    }
+
+    /// <summary>
+    /// Called on Enabled ;)
+    /// </summary>
+    private void OnEnable()
+    {
+        if (TextMeshPro == null) return; // ERROR IF TRUE!
+
+        _textMeshProUGUI.text = "";
+    }
+
+    /// <summary>
+    /// Start is called before the first frame update
+    /// </summary>
+    void Start()
+    {
+        if (TextMeshPro == null) return; // ERROR IF TRUE!
+
+        _textMeshProUGUI.text = "";
+    }
+
+    /// <summary>
+    /// Update is called once per frame
+    /// </summary>
+    void Update()
+    {
+        if (_textMeshProUGUI == null) return; // ERROR IF TRUE!
+
+        if (!isTyping) return;
+
+        secFromLast += Time.deltaTime;
+        if (secFromLast > speed)
+        {
+            secFromLast = 0;
+
+            // Update text
+            string currentText = _textMeshProUGUI.text;
+            
+            // Would like something more hard stop
+            if (currentText.Length == textToPrint.Length)
+            {
+                isTyping = false;
+                return;
+            }
+            currentText += textToPrint[currentText.Length];
+            _textMeshProUGUI.text = currentText;
+        }
+    }
+}
